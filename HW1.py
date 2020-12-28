d085810dcc4ed25b487e7846789f05ffb01ce47d
ines-san <76699289+ines-san@users.noreply.github.com>
2020-12-28 17:58:49 +0100

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,15 @@
+mood = "blah"
+
+if mood == "happy":
+    print("It is great to see you happy.")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Things will get better.")
+elif mood == "excited":
+    print("Awesome!")
+elif mood == "relaxed":
+    print("Stay chill!")
+else:
+    print("I don't recognize this mood.")
+
