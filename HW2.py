868dbab4c9aaaf8d0f74a04ae5050718c6a43648
ines-san <76699289+ines-san@users.noreply.github.com>
2020-12-28 18:11:53 +0100

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,8 @@
+secret = 4
+
+guess = int(input("Make your guess: "))
+
+if guess == secret:
+    print("Congratulations, your guess was correct!")
+else:
+    print("Sorry, your guess was incorrect.")
